2022b3a9d526ca248b67c67df4f88aa23287b173
0sugo <josugo26@gmail.com>
2025-01-06 10:53:17 +0300

    Update views.py

---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from .serializers import RegisterSerializer,LoginSerializer
+from django.contrib.auth.models import User
+
+# Create your views here.
+# user view
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.create(user=user)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+ 
+# login view   
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+    
+    def post(self,request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token,created = Token.objects.get_or_create(user=user)
+            response_data={
+                'token':token.key,
+                'role': self.get_user_role(user),
+            }
+            return Response(response_data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def get_user_role(self, user):
+        """
+        Return the user's role based on their permissions.
+        """
+        if user.is_superuser:
+            return 'superuser'
+        elif user.is_staff:
+            return 'staff'
+        else:
+            return 'patient'
