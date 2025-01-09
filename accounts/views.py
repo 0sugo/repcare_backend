c2997474c704f29a1f8fe1e1b0dd6a35d15db529
0sugo <josugo26@gmail.com>
2025-01-09 23:48:48 +0300

    Update Views.py

--- accounts/views.py
+++ accounts/views.py
@@ -22,6 +22,7 @@ class RegisterView(generics.CreateAPIView):
             token = Token.objects.create(user=user)
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
  
 # login view   
 class LoginView(APIView):
